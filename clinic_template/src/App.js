3991ad0bebe318e84cd00929fd3bacf6b02f8f11
vishal <mgvishalctr@gmail.com>
2024-10-25 23:18:25 +0530

    update

--- clinic_template/src/App.js
+++ clinic_template/src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import Spinner from './components/Spinner';
+import HeaderTopBar from './components/Topbar';
+import Navbar from './components/Navbar';
+import Header from './components/Header';
+import AboutUs from './components/About';
+import Services from './components/Service';
+import Features from './components/Feature';
+import Doctors from './components/Team';
+import Appointment from './components/Appointment';
+import Testimonials from './components/Testimonial';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Spinner/>
+     
+      <HeaderTopBar/>
+      <Navbar/>
+      <Header/>
+      <AboutUs/>
+      <Services/>
+      <Features/>
+      <Doctors/>
+      <Appointment/>
+      <Testimonials/>
+      <Footer/>
+
     </div>
   );
 }
